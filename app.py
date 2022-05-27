3b920fb394b0b4277ac9bc49961493cc629bf501
Cahya Wirawan <cahya.wirawan@gmail.com>
2022-05-27 20:55:04 +0200

    replace sox with librosa, clean up the gradio options

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
 import soundfile as sf
+import librosa
 import torch
 from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
 import gradio as gr
-import sox
 import os
-
-
-def convert(inputfile, outfile):
-    sox_tfm = sox.Transformer()
-    sox_tfm.set_output_format(
-        file_type="wav", channels=1, encoding="signed-integer", rate=16000, bits=16
-    )
-    sox_tfm.build(inputfile, outfile)
 
 
 api_token = os.getenv("API_TOKEN")
@@ -20,6 +12,13 @@ processor = Wav2Vec2Processor.from_pretrained(model_name, use_auth_token=api_tok
 model = Wav2Vec2ForCTC.from_pretrained(model_name, use_auth_token=api_token)
 device = "cuda" if torch.cuda.is_available() else "cpu"
 model = model.to(device)
+
+
+def convert(inputfile, outfile):
+    target_sr = 16000
+    data, sample_rate = librosa.load(inputfile)
+    data = librosa.resample(data, orig_sr=sample_rate, target_sr=target_sr)
+    sf.write(outfile, data, target_sr)
 
 
 def parse_transcription(wav_file):
@@ -39,15 +38,11 @@ output = gr.outputs.Textbox(label="The transcript")
 input_ = gr.inputs.Audio(source="microphone", type="file")
 
 gr.Interface(parse_transcription, inputs=input_,  outputs=[output],
-             server_name="0.0.0.0",
              analytics_enabled=False,
-             show_tips=False,
-             theme='huggingface',
-             layout='vertical',
              title="Multilingual Speech Recognition for Indonesian Languages",
              description="Automatic Speech Recognition Live Demo for Indonesian, Javanese and Sundanese Language",
              article="This demo was built for the project "
                      "<a href='https://github.com/indonesian-nlp/multilingual-asr' target='_blank'>Multilingual Speech Recognition for Indonesian Languages</a>. "
                      "It uses the <a href='https://huggingface.co/indonesian-nlp/wav2vec2-indonesian-javanese-sundanese' target='_blank'>indonesian-nlp/wav2vec2-indonesian-javanese-sundanese</a> model "
-                     "which was fine-tuned on Indonesian Common Voice, Javanese and Sundanese OpenSLR speech datasets.",
-             enable_queue=True).launch( inline=False)
+                     "which was fine-tuned on Indonesian Common Voice, Javanese and Sundanese OpenSLR speech datasets."
+             ).launch( inline=False, server_name="0.0.0.0", show_tips=False, enable_queue=True)
